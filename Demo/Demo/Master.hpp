229683edb82365f3b676904a443a1a321ce5d123
Chen Yonghua <ectheodore@mail.scut.edu.cn>
2017-03-13 02:37:39 +0800

    Multithread Structure

---
+++ Demo/Demo/Master.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <queue>
+#include "Common.h"
+
+
+class Request
+{
+public:
+	Request()
+	{
+
+	}
+
+private:
+	
+};
+
+class Master
+{
+public:
+
+
+
+private:
+	std::queue<
+};
